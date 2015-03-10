08bd56d9fce2d376e28d2962f817f4d6c5d83f14
arguv <gurgen.aramian@gmail.com>
2015-03-10 15:03:21 +0400

    create home pone field and json format

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 $(function () {
     create_new_list();
-
 });
 
 var i = localStorage.length;
 
 function save_person() {
     this.name = $("#input_name").val();
-    this.phone = $("#input_phone").val();
+    this.mobile = $("#input_mobile").val();
+    this.home_phone = $("#input_home_phone").val();
+    this.obj = {'m':this.mobile , 'p':this.home_phone};
 
     if (this.name.length) {
 
-        localStorage.setItem(this.name, this.phone);
+        localStorage.setItem(this.name, JSON.stringify(this.obj) );
 
         $("#person_list").append('<li id="' + this.name + '"><a data-role="button" href="#myPopup" data-rel="popup" onClick="get_id(this);">'+ this.name +'</a><a href="dialog.html" data-role="button" onClick="get_id(this);"></a></li>');
-
 
         $("#person_list").listview();
         $("#person_list").listview("refresh");
@@ -26,7 +26,6 @@ function create_new_list() {
     for (var i = 0; i < localStorage.length; i++) {
 
         name = localStorage.key(i);
-        phone = localStorage.getItem(name);
 
         $("#person_list").append('<li id="' + this.name + '"><a data-role="button" href="#myPopup" data-rel="popup" onClick="get_id(this);">'+ this.name +'</a><a href="dialog.html" data-role="button" onClick="get_id(this);"></a></li>');
     }
@@ -38,10 +37,11 @@ function create_new_list() {
 function get_id(e) {
 
     this.item = $(e).closest("li").attr("id");
-    this.val = localStorage.getItem(this.item);
+    this.val = JSON.parse(localStorage.getItem(this.item));
 
-    $("#myPopup").children('h3').text('mobile: '+this.val);
-    $("#myPopup").children('h4').text('home phone: ');
+    $("#myPopup").children('h2').text('name: '+this.item);
+    $("#myPopup").children('h3').text('mobile: '+this.val['m']);
+    $("#myPopup").children('h4').text('home phone: '+this.val['p']);
 }
 
 function del_person(e) {
@@ -54,5 +54,4 @@ function del_person(e) {
     $("#person_list").html("");
 
     create_new_list();
-
 }
